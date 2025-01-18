ebcc6e5fb3bd3990cdf3857ff128a34facc6b99f
eliuttth-dev <eliuttth@gmail.com>
2025-01-19 00:37:57 +0100

    Fixed expiration time in test

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -218,7 +218,7 @@ func TestGenerateWithExpiration(t *testing.T) {
     t.Fatalf("Failed to initialize handler: %v", err)
   }
 
-  expiration := time.Now().Add(1 * time.Hour)
+  expiration := time.Now().Add(1 * time.Second)
   payload := map[string]interface{}{
     "original_url": "https://github.com/eliuttth-dev",
     "custom_short_url": "eliuth-github-test",
